e94d6d8f25975f97171eb51415744f022a58f5ff
mkrs2404 <kumarmohit2496@gmail.com>
2022-01-19 19:59:27 +0530

    Moved the env variables loading to server.go

--- main.go
+++ main.go
@@ -1,22 +1,7 @@
 package main
 
-import (
-	"log"
-	"os"
-
-	"github.com/joho/godotenv"
-	"github.com/mkrs2404/eKYC/api"
-)
-
-var server = api.Server{}
+import "github.com/mkrs2404/eKYC/api"
 
 func main() {
-
-	//Loading env variables
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error fetching the environment values")
-	} else {
-		server.InitializeDatabase(os.Getenv("DB_HOST"), os.Getenv("DB_NAME"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"))
-	}
+	api.Run()
 }
