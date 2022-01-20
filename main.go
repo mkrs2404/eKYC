3923ba23e01e395d483b33028c63eac9d9d10803
mkrs2404 <kumarmohit2496@gmail.com>
2022-01-20 10:21:31 +0530

    Calling inits of DB and router from main

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/mkrs2404/eKYC/api"
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/mkrs2404/eKYC/api/database"
+	"github.com/mkrs2404/eKYC/api/server"
+)
 
 func main() {
-	api.Run()
+	//api.Run()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error fetching the environment values")
+	} else {
+		database.Connect(os.Getenv("DB_HOST"), os.Getenv("DB_NAME"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"))
+		server.InitializeRouter(os.Getenv("SERVER_ADDR"))
+	}
 }
