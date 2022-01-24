4343be7c735f267cdbb7f949d2c53a8d62a10c54
mkrs2404 <kumarmohit2496@gmail.com>
2022-01-24 14:38:31 +0530

    Merged Client creation and save

--- api/services/client_services.go
+++ api/services/client_services.go
@@ -9,24 +9,21 @@ import (
 	"github.com/mkrs2404/eKYC/database"
 )
 
-//CreateClientFromRequest creates a client object from the provided request data
-func CreateClientFromRequest(signUpRequest resources.SignUpRequest) models.Client {
+/*SaveClient saves the client to the DB and returns back the client and an error(if there's email duplicacy)*/
+func SaveClient(signUpRequest resources.SignUpRequest) (models.Client, error) {
+
+	//Create a client object from the provided request data
 	var client models.Client
 	client.Name = signUpRequest.Name
 	client.Email = signUpRequest.Email
 	client.Plan = GetPlanId(signUpRequest.Plan)
-	return client
-}
 
-/*SaveClient saves the client to the DB
-  and returns back an error if there's email duplicacy*/
-func SaveClient(client *models.Client) error {
-	err := database.DB.Debug().Create(client).Error
+	err := database.DB.Create(&client).Error
 	if err != nil {
 		var pgErr *pgconn.PgError
 		if errors.As(err, &pgErr); pgErr.Code == "23505" {
 			err = errors.New("client already exists")
 		}
 	}
-	return err
+	return client, err
 }
