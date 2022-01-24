90492a1e07f73dcd547620f7fa92bb07f67268a9
mkrs2404 <kumarmohit2496@gmail.com>
2022-01-24 11:51:02 +0530

    Returned the router's handle

--- server/router.go
+++ server/router.go
@@ -4,7 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func InitializeRouter(serverPort string) {
+func InitializeRouter(serverPort string) *gin.Engine {
 
 	//Setting up Router
 	router := gin.Default()
@@ -12,4 +12,5 @@ func InitializeRouter(serverPort string) {
 
 	//Starting up Server
 	router.Run(serverPort)
+	return router
 }
