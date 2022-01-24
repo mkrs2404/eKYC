2a72222b73d7105fa4cd1ce4dcc86e7076fee1c6
mkrs2404 <kumarmohit2496@gmail.com>
2022-01-24 14:39:29 +0530

    Fetching env variable here

--- auth/jwt_token.go
+++ auth/jwt_token.go
@@ -10,7 +10,18 @@ import (
 )
 
 //GenerateToken generates a JWT key with the user Id in the payload
-func GenerateToken(userId int, expiryDelay int) (string, error) {
+func GenerateToken(userId int) (string, error) {
+
+	//Fetching the JWT token delay from env variables
+	tokenExpiryDelay := os.Getenv("TOKEN_EXPIRY_DELAY")
+	if len(tokenExpiryDelay) == 0 {
+		log.Fatal("Token expiry delay not found")
+	}
+
+	expiryDelay, err := strconv.Atoi(tokenExpiryDelay)
+	if err != nil {
+		log.Fatal("Incorrect delay provided")
+	}
 
 	secretKey := os.Getenv("SECRET_KEY")
 	if len(secretKey) == 0 {
