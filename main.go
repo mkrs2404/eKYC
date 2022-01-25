1fa18dd92ab4a205f37ac15e18291be368d5c257
mkrs2404 <kumarmohit2496@gmail.com>
2022-01-25 11:36:38 +0530

    Removed 'else' as per PR review

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"github.com/mkrs2404/eKYC/api/services"
 	"github.com/mkrs2404/eKYC/database"
 	"github.com/mkrs2404/eKYC/server"
-	_ "github.com/mkrs2404/eKYC/server"
 	"gorm.io/gorm/logger"
 )
 
@@ -16,9 +15,8 @@ func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error fetching the environment values")
-	} else {
-		database.Connect(os.Getenv("DB_HOST"), os.Getenv("DB_NAME"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), logger.Error)
-		services.SeedPlanData()
-		server.InitializeRouter()
 	}
+	database.Connect(os.Getenv("DB_HOST"), os.Getenv("DB_NAME"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), logger.Error)
+	services.SeedPlanData()
+	server.InitializeRouter()
 }
