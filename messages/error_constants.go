4a8b395d0dcb7f357c3f89d2fc0d053db98206e9
mkrs2404 <kumarmohit2496@gmail.com>
2022-01-25 19:19:07 +0530

    Added constants

--- messages/error_constants.go
+++ messages/error_constants.go
@@ -1,3 +1,6 @@
 package messages
 
 const SIGN_UP_FAILED = "sign up failed"
+const PROVIDE_PROPER_AUTH_HEADER = "Must provide Authorization header with format `Bearer {token}`"
+const UNAUTHORIZED = "unauthorized"
+const FILE_UPLOAD_FAILED = "file upload failed"
