69320e65cf6b8c1976f4e479bf4e7d058d892cd6
mkrs2404 <kumarmohit2496@gmail.com>
2022-02-02 11:24:13 +0530

    Modified error

--- messages/error_constants.go
+++ messages/error_constants.go
@@ -1,7 +1,7 @@
 package messages
 
 const SIGN_UP_FAILED = "sign up failed"
-const PROVIDE_PROPER_AUTH_HEADER = "Must provide Authorization header with format `Bearer {token}`"
+const PROVIDE_PROPER_AUTH_HEADER = "must provide Authorization header with format `Bearer {token}`"
 const UNAUTHORIZED = "unauthorized"
 const FILE_UPLOAD_FAILED = "file upload failed"
 const BUCKET_CREATION_FAILED = "bucket creation failed"
