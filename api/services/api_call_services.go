d32d0c6c5cecf68fcd0d57e13ddf51c75c14854b
mkrs2404 <kumarmohit2496@gmail.com>
2022-02-03 18:26:24 +0530

    Added validation and updation services

--- api/services/api_call_services.go
+++ api/services/api_call_services.go
@@ -6,14 +6,27 @@ import (
 )
 
 //SaveApiCall saves the api calls into the database
-func SaveApiCall(matchResult int, apiType string, clientId uint) error {
+func SaveApiCall(matchResult int, apiType string, clientId uint) (models.Api_Calls, error) {
 
 	var api_call models.Api_Calls
-
 	api_call.MatchResult = float32(matchResult)
 	api_call.Type = apiType
 	api_call.ClientID = clientId
-
 	err := database.DB.Create(&api_call).Error
+	return api_call, err
+}
+
+func UpdateApiCall(api_call models.Api_Calls, matchResult int) (models.Api_Calls, error) {
+
+	database.DB.First(&api_call)
+	api_call.MatchResult = float32(matchResult)
+	err := database.DB.Save(&api_call).Error
+	return api_call, err
+}
+
+func ValidateMatchId(matchId, clientId int) error {
+
+	var api_call models.Api_Calls
+	err := database.DB.Where("id = ? AND client_id = ?", matchId, clientId).First(&api_call).Error
 	return err
 }
