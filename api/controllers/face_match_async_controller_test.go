dc64658510d1ae5132e925ab39c21f7f9558c999
mkrs2404 <kumarmohit2496@gmail.com>
2022-02-07 13:22:49 +0530

    Added get score tests

--- api/controllers/face_match_async_controller_test.go
+++ api/controllers/face_match_async_controller_test.go
@@ -1,21 +1,15 @@
 package controllers
 
 import (
-	"context"
 	"fmt"
-	"log"
 	"net/http"
 	"net/http/httptest"
-	"path/filepath"
 	"strings"
 	"testing"
 
 	"github.com/gin-gonic/gin"
-	"github.com/minio/minio-go/v7"
 	"github.com/mkrs2404/eKYC/api/middlewares"
 	"github.com/mkrs2404/eKYC/api/services"
-	"github.com/mkrs2404/eKYC/database"
-	"github.com/mkrs2404/eKYC/minio_client"
 )
 
 var asyncFaceMatchTestData = []struct {
@@ -38,9 +32,29 @@ var asyncFaceMatchTestData = []struct {
 	},
 }
 
+var asyncGetScoreTestData = []struct {
+	body         string
+	expectedCode int
+}{
+	//Positive case
+	{
+		expectedCode: 200,
+	},
+	//Invalid job id
+	{
+		body:         `{"job_id":11}`,
+		expectedCode: 400,
+	},
+	//Missing job id
+	{
+		body:         `{"job_id": }`,
+		expectedCode: 400,
+	},
+}
+
 const asyncFaceMatchUrl = "/api/v1/face-match-async"
 
-// const asyncGetFaceMatchScoreUrl = "/api/v1/get-score"
+const asyncGetFaceMatchScoreUrl = "/api/v1/get-score"
 
 func TestAsyncFaceMatchClient(t *testing.T) {
 	gin.SetMode(gin.TestMode)
@@ -58,27 +72,10 @@ func TestAsyncFaceMatchClient(t *testing.T) {
 		}
 		ctx.Set("client", client)
 
-		filePath1 := "../../test_assets/db5ed785-e54e-4ffb-9ed5-0653aea87217.png"
-		fileName1 := filepath.Base(filePath1)
-		filePath2 := "../../test_assets/f928e240-42da-490c-a4c2-14aac382f03b.png"
-		fileName2 := filepath.Base(filePath2)
-
 		//Setting separate bucket name for tests
 		ctx.Set("testBucket", "test")
 
-		//Uploading the files to minio
-		fileInfo1, err1 := services.UploadToMinio(client.ID, fileName1, "face", filePath1, context.Background(), "")
-		fileInfo2, err2 := services.UploadToMinio(client.ID, fileName2, "face", filePath2, context.Background(), "")
-		if err1 != nil || err2 != nil {
-			log.Fatal("Upload to minio failed")
-		}
-
-		//Saving file's metadata to the database
-		fileUUID1, err1 := services.SaveFile(fileInfo1.Bucket, fileInfo1.Key, fileInfo1.Size, "face", client.ID)
-		fileUUID2, err2 := services.SaveFile(fileInfo2.Bucket, fileInfo2.Key, fileInfo2.Size, "face", client.ID)
-		if err1 != nil || err2 != nil {
-			log.Fatal("DB save failed")
-		}
+		fileUUID1, fileUUID2, fileInfo1, fileInfo2 := services.SetupImageUpload(client, "face", "face")
 
 		if data.body == "" {
 			data.body = fmt.Sprintf(`{"image1":"%s", "image2":"%s"}`, fileUUID1, fileUUID2)
@@ -93,17 +90,49 @@ func TestAsyncFaceMatchClient(t *testing.T) {
 			t.Errorf("Expected %d, Got %d ", data.expectedCode, resRecorder.Code)
 		}
 
-		//Clearing up test DB
-		database.DB.Exec("DELETE FROM api_calls")
-		database.DB.Exec("DELETE FROM files")
-		database.DB.Exec("DELETE FROM clients")
-
-		//Deleting the test images uploaded to minio
-		minio_client.Minio.RemoveObject(context.Background(), services.BucketName, fileInfo1.Key, minio.RemoveObjectOptions{})
-		minio_client.Minio.RemoveObject(context.Background(), services.BucketName, fileInfo2.Key, minio.RemoveObjectOptions{})
+		Clear(fileInfo1.Key, fileInfo2.Key)
 	}
 }
 
 func TestAsyncGetFaceMatchScore(t *testing.T) {
 
+	gin.SetMode(gin.TestMode)
+	router := gin.Default()
+
+	router.POST(asyncGetFaceMatchScoreUrl, middlewares.AuthRequired(), GetFaceMatchScore)
+
+	for _, data := range asyncGetScoreTestData {
+		resRecorder := httptest.NewRecorder()
+		ctx, _ := gin.CreateTestContext(resRecorder)
+
+		token, client, err := services.SetupClient()
+		if err != nil {
+			t.Fatal("Error setting up the client")
+		}
+		ctx.Set("client", client)
+
+		//Setting separate bucket name for tests
+		ctx.Set("testBucket", "test")
+
+		fileUUID1, fileUUID2, fileInfo1, fileInfo2 := services.SetupImageUpload(client, "face", "face")
+
+		api_call, err := services.SetupAsyncFaceMatch(fileUUID1.String(), fileUUID2.String(), "face-match", client)
+		if err != nil {
+			t.Fatal("Error setting up the Async face match")
+		}
+		if data.body == "" {
+			data.body = fmt.Sprintf(`{"job_id":%d}`, api_call.ID)
+		}
+		ctx.Request, _ = http.NewRequest(http.MethodPost, asyncGetFaceMatchScoreUrl, strings.NewReader(data.body))
+
+		//Setting the authorization token
+		ctx.Request.Header.Set("Authorization", token)
+		GetFaceMatchScore(ctx)
+
+		if resRecorder.Code != data.expectedCode {
+			t.Errorf("Expected %d, Got %d ", data.expectedCode, resRecorder.Code)
+		}
+
+		Clear(fileInfo1.Key, fileInfo2.Key)
+	}
 }
