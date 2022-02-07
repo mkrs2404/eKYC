40264bcee6e1bd01c33d0301ce8ee6960e1e2fbd
mkrs2404 <kumarmohit2496@gmail.com>
2022-02-07 13:20:14 +0530

    Refactored code into separate functions

--- api/controllers/face_match_controller_test.go
+++ api/controllers/face_match_controller_test.go
@@ -3,10 +3,8 @@ package controllers
 import (
 	"context"
 	"fmt"
-	"log"
 	"net/http"
 	"net/http/httptest"
-	"path/filepath"
 	"strings"
 	"testing"
 
@@ -56,27 +54,10 @@ func TestFaceMatchClient(t *testing.T) {
 		}
 		ctx.Set("client", client)
 
-		filePath1 := "../../test_assets/db5ed785-e54e-4ffb-9ed5-0653aea87217.png"
-		fileName1 := filepath.Base(filePath1)
-		filePath2 := "../../test_assets/f928e240-42da-490c-a4c2-14aac382f03b.png"
-		fileName2 := filepath.Base(filePath2)
-
 		//Setting separate bucket name for tests
 		ctx.Set("testBucket", "test")
 
-		//Uploading the files to minio
-		fileInfo1, err1 := services.UploadToMinio(client.ID, fileName1, "face", filePath1, context.Background(), "")
-		fileInfo2, err2 := services.UploadToMinio(client.ID, fileName2, "face", filePath2, context.Background(), "")
-		if err1 != nil || err2 != nil {
-			log.Fatal("Upload to minio failed")
-		}
-
-		//Saving file's metadata to the database
-		fileUUID1, err1 := services.SaveFile(fileInfo1.Bucket, fileInfo1.Key, fileInfo1.Size, "face", client.ID)
-		fileUUID2, err2 := services.SaveFile(fileInfo2.Bucket, fileInfo2.Key, fileInfo2.Size, "face", client.ID)
-		if err1 != nil || err2 != nil {
-			log.Fatal("DB save failed")
-		}
+		fileUUID1, fileUUID2, fileInfo1, fileInfo2 := services.SetupImageUpload(client, "face", "face")
 
 		if data.body == "" {
 			data.body = fmt.Sprintf(`{"image1":"%s", "image2":"%s"}`, fileUUID1, fileUUID2)
@@ -87,18 +68,21 @@ func TestFaceMatchClient(t *testing.T) {
 		ctx.Request.Header.Set("Authorization", token)
 		FaceMatchClient(ctx)
 
-		router.ServeHTTP(resRecorder, ctx.Request)
 		if resRecorder.Code != data.expectedCode {
 			t.Errorf("Expected %d, Got %d ", data.expectedCode, resRecorder.Code)
 		}
 
-		//Clearing up test DB
-		database.DB.Exec("DELETE FROM api_calls")
-		database.DB.Exec("DELETE FROM files")
-		database.DB.Exec("DELETE FROM clients")
-
-		//Deleting the test images uploaded to minio
-		minio_client.Minio.RemoveObject(context.Background(), services.BucketName, fileInfo1.Key, minio.RemoveObjectOptions{})
-		minio_client.Minio.RemoveObject(context.Background(), services.BucketName, fileInfo2.Key, minio.RemoveObjectOptions{})
+		Clear(fileInfo1.Key, fileInfo2.Key)
 	}
+}
+
+func Clear(objectName1, objectName2 string) {
+	//Clearing up test DB
+	database.DB.Exec("DELETE FROM api_calls")
+	database.DB.Exec("DELETE FROM files")
+	database.DB.Exec("DELETE FROM clients")
+
+	//Deleting the test images uploaded to minio
+	minio_client.Minio.RemoveObject(context.Background(), services.BucketName, objectName1, minio.RemoveObjectOptions{})
+	minio_client.Minio.RemoveObject(context.Background(), services.BucketName, objectName2, minio.RemoveObjectOptions{})
 }
