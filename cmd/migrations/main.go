1e2e6dce954ef88fab379011f3d35fe87df9f97f
mkrs2404 <kumarmohit2496@gmail.com>
2022-02-14 17:18:21 +0530

    Added migrator

--- cmd/migrations/main.go
+++ cmd/migrations/main.go
@@ -7,9 +7,7 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/mkrs2404/eKYC/app/database"
 	"github.com/mkrs2404/eKYC/app/helper"
-	"github.com/mkrs2404/eKYC/app/minio_client"
-	"github.com/mkrs2404/eKYC/app/redis_client"
-	"github.com/mkrs2404/eKYC/app/server"
+	"github.com/mkrs2404/eKYC/app/models"
 	"gorm.io/gorm/logger"
 )
 
@@ -21,13 +19,14 @@ func main() {
 
 	// err := godotenv.Load("../../.env")
 	err := godotenv.Load()
+
 	if err != nil {
 		log.Fatal("Error fetching the environment values")
 	}
 	database.Connect(os.Getenv("DB_HOST"), os.Getenv("DB_NAME"), os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), logger.Error)
-	// services.SeedPlanData()
-	minio_client.InitializeMinio(os.Getenv("MINIO_SERVER"), os.Getenv("MINIO_USER"), os.Getenv("MINIO_PWD"))
-	redis_client.InitializeRedis(os.Getenv("REDIS_SERVER"), os.Getenv("REDIS_PASSWORD"))
-	server.InitializeRouter()
+
+	//Migrating tables to the database
+	database.DB.AutoMigrate(&models.Plan{}, &models.Client{}, &models.File{}, &models.Api_Calls{})
+
 }
